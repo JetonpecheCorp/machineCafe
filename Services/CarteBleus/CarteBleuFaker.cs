7b6e92ac18b0010b5dba77f28bf19d69cc935f75
jetonpeche <nicolas.np63@gmail.com>
2024-07-05 09:58:26 +0200

    carte bleu

---
+++ Services/CarteBleus/CarteBleuFaker.cs
@@ -0,0 +1,17 @@
+﻿namespace Services.CarteBleus;
+
+public class CarteBleuFaker : ICarteBleu
+{
+    public required string Numero { get; init; }
+    public required double NbArgent { get; set; }
+
+    public bool Debiter(EPiece _piece)
+    {
+        if (NbArgent < (double)_piece)
+            return false;
+
+        NbArgent -= (double)_piece;
+
+        return true;
+    }
+}
---
+++ Services/CarteBleus/ICarteBleu.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.CarteBleus;
+
+public interface ICarteBleu
+{
+    string Numero { get; init; }
+    double NbArgent { get; set; }
+
+    bool Debiter(EPiece _piece);
+}
