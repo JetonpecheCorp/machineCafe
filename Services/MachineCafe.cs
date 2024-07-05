217dfa4d380ceb38f6bee07dcea8d3111ff326fd
jetonpeche <nicolas.np63@gmail.com>
2024-07-05 16:07:07 +0200

    builder machine cafe

--- Services/MachineCafe.cs
+++ Services/MachineCafe.cs
@@ -5,25 +5,41 @@ namespace Services;
 
 public class MachineCafe
 {
-    public IHardware Hardware { get; private init; }
-    public IHardwareCarteBleu? HardwareCarteBleu { get; private init; } = null;
+    public IHardware Hardware { get; private set; }
+    public IHardwareCarteBleu? HardwareCarteBleu { get; private set; } = null;
 
     public byte PrixCafe { get; }
     public uint ArgentTotal { get; private set; }
 
     ICarteBleu? carteBleu;
 
-    public MachineCafe(IHardware _hardware, IHardwareCarteBleu? _hardwareCarteBleu = null)
+    public MachineCafe()
+    {
+        PrixCafe = (byte)EPiece._50Centime;
+    }
+
+    public MachineCafe AjouterHadwareCarteBleu(IHardwareCarteBleu _hardwareCarteBleu)
+    {
+        HardwareCarteBleu = _hardwareCarteBleu;
+        HardwareCarteBleu.CallbackEnregistrerCarteBleu = PayerSansContact;
+
+        return this;
+    }
+
+    public MachineCafe ModifierHardware(IHardware _hardware)
     {
         Hardware = _hardware;
         Hardware.CallbackInsertionPiece = Inserer;
 
-        HardwareCarteBleu = _hardwareCarteBleu;
+        return this;
+    }
 
-        if(HardwareCarteBleu is not null)
-            HardwareCarteBleu.CallbackEnregistrerCarteBleu = PayerSansContact;
+    public MachineCafe Build()
+    {
+        if(Hardware is null)
+            ModifierHardware(new HardwareFaker());
 
-        PrixCafe = (byte)EPiece._50Centime;
+        return this;
     }
 
     void Inserer(EPiece _piece)
@@ -36,8 +52,11 @@ public class MachineCafe
         Hardware.CoulerCafe();
     }
 
-    void PayerSansContact(ICarteBleu _carteBleu)
+    void PayerSansContact(ICarteBleu? _carteBleu)
     {
+        if (_carteBleu is null)
+            return;
+
         if (ArgentTotal > 0)
             ArgentTotal = 0;
 
--- Tests/MVP1Test.cs
+++ Tests/MVP1Test.cs
@@ -1,24 +1,22 @@
 using Services;
 using Services.CarteBleus;
-using Services.Hardwares;
 
 namespace Tests;
 
 public class MVP1Test
 {
-    readonly MachineCafe machineCafe;
+    public MachineCafe machineCafe { get; init; }
 
     public MVP1Test()
     {
-        machineCafe = new(
-            new HardwareFaker(),
-            new HardwareCarteBleuFaker()
-        );
+        machineCafe = new();
     }
 
     [Fact]
     public void Payer_sans_contact_test()
     {
+        machineCafe.AjouterHadwareCarteBleu(new HardwareCarteBleuFaker()).Build();
+
         CarteBleuFaker cb = new("1234-1234-1234-1234", 10);
         machineCafe.HardwareCarteBleu!.SimulerPayementSansContact(cb);
 
@@ -28,6 +26,8 @@ public class MVP1Test
     [Fact]
     public void Payer_sans_contact_pas_assez_argent_test()
     {
+        machineCafe.AjouterHadwareCarteBleu(new HardwareCarteBleuFaker()).Build();
+
         CarteBleuFaker cb = new("1234-1234-1234-1234", 0);
         machineCafe.HardwareCarteBleu!.SimulerPayementSansContact(cb);
 
@@ -37,6 +37,8 @@ public class MVP1Test
     [Fact]
     public void Payer_sans_contact_et_avec_une_piece_test()
     {
+        machineCafe.AjouterHadwareCarteBleu(new HardwareCarteBleuFaker()).Build();
+
         CarteBleuFaker cb = new("1234-1234-1234-1234", 10);
         machineCafe.Hardware.SimulerInsertionPiece(EPiece._20Centime);
 
@@ -48,6 +50,7 @@ public class MVP1Test
     [Fact]
     public void Servir_cafe_multiple_piece_test()
     {
+        machineCafe.Build();
         machineCafe.Hardware.SimulerInsertionPiece(EPiece._20Centime);
         machineCafe.Hardware.SimulerInsertionPiece(EPiece._20Centime);
         machineCafe.Hardware.SimulerInsertionPiece(EPiece._10Centime);
@@ -58,6 +61,7 @@ public class MVP1Test
     [Fact]
     public void Servir_deux_cafe_test()
     {
+        machineCafe.Build();
         machineCafe.Hardware.SimulerInsertionPiece(EPiece._50Centime);
         machineCafe.Hardware.SimulerInsertionPiece(EPiece._50Centime);
 
@@ -72,6 +76,7 @@ public class MVP1Test
     [InlineData(EPiece._20Centime)]
     public void Pas_assez_argent_servir_cafe_test(EPiece _piece)
     {
+        machineCafe.Build();
         machineCafe.Hardware.SimulerInsertionPiece(_piece);
 
         Assert.Equal(0, (int)machineCafe.Hardware.NbCafeFabrique);
@@ -83,6 +88,7 @@ public class MVP1Test
     [InlineData(EPiece._2Euro)]
     public void Trop_argent_servir_cafe_test(EPiece _piece)
     {
+        machineCafe.Build();
         machineCafe.Hardware.SimulerInsertionPiece(_piece);
 
         Assert.Equal(1, (int)machineCafe.Hardware.NbCafeFabrique);
